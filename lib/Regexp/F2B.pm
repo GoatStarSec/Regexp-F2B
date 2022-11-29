36b893961f23320cfedbb71265d9f53f1b7ef828
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2022-11-28 21:15:40 -0600

    add initial bits for more complex items

--- lib/Regexp/F2B.pm
+++ lib/Regexp/F2B.pm
@@ -89,50 +89,138 @@ sub new {
 		$int++;
 	}
 
+	if ( !defined( $opts{regexp} ) ) {
+		$opts{pre_regexp} = [];
+	}
+	else {
+		if ( ref( $opts{regexp} ) ne 'ARRAY' ) {
+			die( 'regexp is a ' . ref( $opts{regexp} ) . ' and not a array' );
+		}
+	}
+
 	my $self = {
-		lines     => $opts{lines},
-		log_lines => [],
-		regexp    => $opts{regexp},
+		lines          => $opts{lines},
+		log_lines      => [],
+		pre_regexp     => $opts{pre_regexp},
+		regexp         => $opts{regexp},
+		has_mlf_id     => {},
+		has_mlf_forget => {},
+		has_no_fail    => {},
 	};
 	bless $self;
 
 	# process each regexp
-	$int = 0;
-	while ( defined( $self->{regexp}[$int] ) ) {
-		if ( $self->{regexp}[$int] =~ /\<HOST\>/ ) {
-			$self->{regexp}[$int] =~ s/\<HOST\>/($IPv4_re|$IPv6_re|[a-zA-Z][a-zA-Z\-0-9\.]*[a-zA-Z\-0-9]+)/;
-		}elsif( $self->{regexp}[$int] =~ /\<ADDR\>/ ) {
-			$self->{regexp}[$int] =~ s/\<ADDR\>/($IPv4_re|$IPv6_re)/;
-		}elsif( $self->{regexp}[$int] =~ /\<CIDR\>/ ) {
-			$self->{regexp}[$int] =~ s/\<CIDR\>/($IPv4_re\/\\b([1-9]|[12][0-9]|3[0-2])\\b|$IPv6_re\/\\b([1-9]|[1-9][0-9]|1[01][0-9]|12[0-8])\\b)/;
-		}elsif( $self->{regexp}[$int] =~ /\<SUBNET\>/ ) {
-			$self->{regexp}[$int] =~ s/\<SUBNET\>/($IPv4_re|$IPv6_re|$IPv4_re\/\\b([1-9]|[12][0-9]|3[0-2])\\b|$IPv6_re\/\\b([1-9]|[1-9][0-9]|1[01][0-9]|12[0-8])\\b)/;
-		}
-		elsif ( $self->{regexp}[$int] =~ /\<IP4\>/ ) {
-			$self->{regexp}[$int] =~ s/\<IP4\>/($IPv4_re)/;
-		}
-		elsif ( $self->{regexp}[$int] =~ /\<IP6\>/ ) {
-			$self->{regexp}[$int] =~ s/\<IP6\>/($IPv6_re)/;
-		}
-		elsif ( $self->{regexp}[$int] =~ /\<DNS\>/ ) {
-			$self->{regexp}[$int] =~ s/\<DNS\>/([a-zA-Z][a-zA-Z\-0-9\.]*[a-zA-Z\-0-9]+)/;
-		}
+	my @items = ( 'regexp', 'pre_regexp' );
+	foreach my $regexp (@items) {
+		$int = 0;
+		while ( defined( $self->{$regexp}[$int] ) ) {
+			if ( $self->{$regexp}[$int] =~ /\<HOST\>/ ) {
+				$self->{$regexp}[$int] =~ s/\<HOST\>/($IPv4_re|$IPv6_re|[a-zA-Z][a-zA-Z\-0-9\.]*[a-zA-Z\-0-9]+)/;
+			}
+			elsif ( $self->{$regexp}[$int] =~ /\<ADDR\>/ ) {
+				$self->{$regexp}[$int] =~ s/\<ADDR\>/($IPv4_re|$IPv6_re)/;
+			}
+			elsif ( $self->{$regexp}[$int] =~ /\<CIDR\>/ ) {
+				$self->{$regexp}[$int]
+					=~ s/\<CIDR\>/($IPv4_re\/\\b([1-9]|[12][0-9]|3[0-2])\\b|$IPv6_re\/\\b([1-9]|[1-9][0-9]|1[01][0-9]|12[0-8])\\b)/;
+			}
+			elsif ( $self->{$regexp}[$int] =~ /\<SUBNET\>/ ) {
+				$self->{$regexp}[$int]
+					=~ s/\<SUBNET\>/($IPv4_re|$IPv6_re|$IPv4_re\/\\b([1-9]|[12][0-9]|3[0-2])\\b|$IPv6_re\/\\b([1-9]|[1-9][0-9]|1[01][0-9]|12[0-8])\\b)/;
+			}
+			elsif ( $self->{$regexp}[$int] =~ /\<IP4\>/ ) {
+				$self->{$regexp}[$int] =~ s/\<IP4\>/($IPv4_re)/;
+			}
+			elsif ( $self->{$regexp}[$int] =~ /\<IP6\>/ ) {
+				$self->{$regexp}[$int] =~ s/\<IP6\>/($IPv6_re)/;
+			}
+			elsif ( $self->{$regexp}[$int] =~ /\<DNS\>/ ) {
+				$self->{$regexp}[$int] =~ s/\<DNS\>/([a-zA-Z][a-zA-Z\-0-9\.]*[a-zA-Z\-0-9]+)/;
+			}
 
-		if ( $self->{regexp}[$int] =~ /\<SKIPLINES\>/ ) {
-			$self->{regexp}[$int] =~ s/\<SKIPLINES\>/.*/g;
-		}
+			if ( $self->{$regexp}[$int] =~ /\<SKIPLINES\>/ ) {
+				$self->{$regexp}[$int] =~ s/\<SKIPLINES\>/.*/g;
+			}
 
-		if ( $self->{regexp}[$int] !~ /\$$/ ) {
-			$self->{regexp}[$int] = $self->{regexp}[$int] . '.*$';
-		}
-		if ( $self->{regexp}[$int] !~ /^\^/ ) {
-			$self->{regexp}[$int] = '^.*' . $self->{regexp}[$int];
-		}
+			# remove F-USER bits as those are pointless
+			if ( $self->{$regexp}[$int] =~ /\<F-[ALT\_]*USER[0-9]\>/ ) {
+				$self->{$regexp}[$int] =~ s/\<F-[ALT\_]*USER[0-9]\>//g;
+			}
+			if ( $self->{$regexp}[$int] =~ /\<\/F-[ALT\_]*USER[0-9]\>/ ) {
+				$self->{$regexp}[$int] =~ s/\<\/F-[ALT\_]*USER[0-9]\>//g;
+			}
 
-		$int++;
+			# find F-MLFID lines
+			if ( $self->{$regexp}[$int] =~ /\<F-MLFID\>/ ) {
+				$self->{$regexp}[$int] =~ s/\<F-MLFID>/(/g;
+				$self->{$regexp}[$int] =~ s/\<\/F-MLFID\>/)/g;
+				$self->{hash_mlf_id}{$int} = 1;
+			}
+			elsif ( $self->{$regexp}[$int] =~ /\<\/F-MLFID\>/ ) {
+				die(      $regexp . '['
+						. $int
+						. '] contains </F-MLFID> but no <F-MLFID>... '
+						. Dumper( $self->{regexp}[$int] ) );
+				$self->{hash_mlf_id}{$int} = 1;
+			}
+			else {
+				$self->{hash_mlf_id}{$int} = 0;
+			}
+
+			# find F-NOFAIL lines
+			if ( $self->{$regexp}[$int] =~ /\<F-NOFAIL\>/ ) {
+				$self->{$regexp}[$int] =~ s/\<F-NOFAIL>/(/g;
+				$self->{$regexp}[$int] =~ s/\<\/F-NOFAIL\>/)/g;
+				$self->{hash_no_fail}{$int} = 1;
+			}
+			elsif ( $self->{$regexp}[$int] =~ /\<\/F-NOFAIL\>/ ) {
+				die(      $regexp . '['
+						. $int
+						. '] contains </F-NOFAIL> but no <F-NOFAIL>... '
+						. Dumper( $self->{regexp}[$int] ) );
+			}
+			else {
+				$self->{hash_no_fail}{$int} = 0;
+			}
+
+			# find F-MLFFORGET lines
+			if ( $self->{$regexp}[$int] =~ /\<F-MLFFORGET\>/ ) {
+				$self->{$regexp}[$int] =~ s/\<F-MLFFORGET>//g;
+				$self->{$regexp}[$int] =~ s/\<\/F-MLFFORGET\>//g;
+				$self->{hash_mlf_forget}{$int} = 1;
+			}
+			elsif ( $self->{regexp}[$int] =~ /\<\/F-MLFFORGET\>/ ) {
+				die(      $regexp . '['
+						. $int
+						. '] contains </F-MLFFORGET> but no <F-MLFFORGET>... '
+						. Dumper( $self->{regexp}[$int] ) );
+			}
+			else {
+				$self->{hash_mlf_forget}{$int} = 0;
+			}
+
+			if ( $self->{$regexp}[$int] !~ /\$$/ ) {
+				$self->{$regexp}[$int] = $self->{regexp}[$int] . '.*$';
+			}
+			if ( $self->{$regexp}[$int] !~ /^\^/ ) {
+				$self->{$regexp}[$int] = '^.*' . $self->{regexp}[$int];
+			}
+
+			$int++;
+		}
 	}
 
 	return $self;
+}
+
+=head2 new_from_f2b_filter
+
+=cut
+
+sub new_from_f2b_filter{
+	my ( $blank, %opts ) = @_;
+
+
 }
 
 =head2 proc_lines
